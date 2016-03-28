529159c1aa8004866f8a8b334aca40d863064bd9
Lovish Dogra <lovishdogra7@gmail.com>
2016-03-29 03:44:10 +0530

    Feed wall implements

---
+++ parseserverdemo/Cell.swift
@@ -0,0 +1,18 @@
+//
+//  Cell.swift
+//  parseserverdemo
+//
+//  Created by Lovish Dogra on 29/03/16.
+//  Copyright © 2016 Lovish Dogra. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet var imagePostView: UIImageView!
+    @IBOutlet var usernameLabel: UILabel!
+    @IBOutlet var messageLabel: UILabel!
+    
+
+}
